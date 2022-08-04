e50f4bf545c80e99f0841e515707ad4a48fe5c4c
Sanjin-Git <ripsanja@gmail.com>
2022-08-04 10:04:45 +0200

    add reg positive

--- cypress/e2e/register_positive.cy.js
+++ cypress/e2e/register_positive.cy.js
@@ -1,5 +1,8 @@
 /// <reference types="Cypress" />
 
+import { faker } from '@faker-js/faker';
+const randomEmail = faker.internet.email();
+
 describe("Positive test cases - register", ( )=> {
     it ("Visit gallery app page", () => {
         cy.visit("/");
@@ -12,7 +15,7 @@ describe("Positive test cases - register", ( )=> {
     it("Create registration - vaild input", () => {
         cy.get('#first-name').type("Test445");
         cy.get('#last-name').type("Test445");
-        cy.get('#email').type("t1e2s@test.com");
+        cy.get('#email').type("t1e2t4@test.com");
         cy.get('#password').type("test2022");
         cy.get('#password-confirmation').type("test2022");
         cy.get('.form-check-input').check();
@@ -30,7 +33,7 @@ describe("Positive test cases - register", ( )=> {
     it("Create registration with one-char first name and last name", () => {
         cy.get('#first-name').clear().type("*");
         cy.get('#last-name').clear().type("2");
-        cy.get('#email').clear().type("t0e1s2ttest1.com");
+        cy.get('#email').clear().type("t0e1s2t3t@test1.com");
         cy.get('#password').clear().type("test2022");
         cy.get('#password-confirmation').clear().type("test2022");
         cy.get('.form-check-input').check();
